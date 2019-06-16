a97327223b4591b69336bb99423befae93bf706b
Ryota Ikezawa <pavegy@gmail.com>
2019-06-17 02:28:45 +0900

    fix configure

--- frontend/javascripts/components/MainContainer.tsx
+++ frontend/javascripts/components/MainContainer.tsx
@@ -1,35 +1,52 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 interface DeliveryCompany {
-  url: string
-  id: number
-  contractor: string
+  url: string;
+  id: number;
+  contractor: string;
 }
 
-type State = { delivery_companies: Array<DeliveryCompany>, selected: number, }
+interface State {
+  deliveryCompanies: DeliveryCompany[];
+  selected: number;
+}
 
 class MainContainer extends React.Component<{}, State> {
-  constructor(props: Object) {
+  private constructor(props: Record<string, any>) {
     super(props);
     this.state = {
-      delivery_companies: [],
-      selected: 1,
-    }
+      deliveryCompanies: [],
+      selected: 1
+    };
   }
 
-  componentDidMount(): void {
-    axios.get('http://localhost:3000/delivery_companies').then(res => {
-      const items: Array<DeliveryCompany> = res.data;
-      this.setState({delivery_companies: items})
-    }).catch((data) => {
-      console.log(data)
-    })
+  public componentDidMount(): void {
+    axios
+      .get("http://localhost:3000/delivery_companies")
+      .then(res => {
+        const items: DeliveryCompany[] = res.data;
+        this.setState({ deliveryCompanies: items });
+      })
+      .catch(data => {
+        console.log(data);
+        return;
+      });
+    return;
   }
 
-  render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-    const list: Array<JSX.Element> = [];
-    this.state.delivery_companies.map((dc: DeliveryCompany) => {
+  public render():
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | string
+    | number
+    | {}
+    | React.ReactNodeArray
+    | React.ReactPortal
+    | boolean
+    | null
+    | undefined {
+    const list: JSX.Element[] = [];
+    this.state.deliveryCompanies.map((dc: DeliveryCompany) => {
       list.push(
         <div>
           <label>
@@ -37,9 +54,12 @@ class MainContainer extends React.Component<{}, State> {
               type="radio"
               value={dc.id}
               checked={dc.id == this.state.selected}
-              onChange={() => this.setState({selected: dc.id})} />{dc.contractor}
+              onChange={() => this.setState({ selected: dc.id })}
+            />
+            {dc.contractor}
           </label>
-        </div>)
+        </div>
+      );
     });
 
     return (
@@ -49,8 +69,8 @@ class MainContainer extends React.Component<{}, State> {
           <button type="submit">選択する</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default MainContainer
+export default MainContainer;
--- frontend/javascripts/root/entry.tsx
+++ frontend/javascripts/root/entry.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import ReactDOM from 'react-dom'
-import MainContainer from "../components/MainContainer"
+import ReactDOM from "react-dom";
+import MainContainer from "@js/components/MainContainer";
 
-const element = (<div className="App"><MainContainer /></div>);
-
-ReactDOM.render(
-  element,
-  document.getElementById('app')
+const element = (
+  <div className="App">
+    <MainContainer />
+  </div>
 );
+
+ReactDOM.render(element, document.getElementById("app"));
